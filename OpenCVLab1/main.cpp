6b120a6959bc3fdc038f066379f98ea70d0e28b4
Raazia Tariq <89384079+RaaziaTarique@users.noreply.github.com>
2022-03-26 17:28:08 +0100

    indendation

--- OpenCVLab1/main.cpp
+++ OpenCVLab1/main.cpp
@@ -198,49 +198,49 @@ public:void task3(cv::Mat src){
     cv::waitKey(0);
 }
     
-
-
+    
+    
     void drawHistogram(cv::Mat src) {
         vector<Mat> bgr_planes;
-            split(src, bgr_planes );
-            int histSize = 256;
-            float range[] = { 0, 255 }; //the upper boundary is exclusive
-            const float* histRange[] = { range };
-            bool uniform = true, accumulate = false;
-            Mat b_hist, g_hist, r_hist;
-            calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, histRange, uniform, accumulate );
-            calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, histRange, uniform, accumulate );
-            calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, histRange, uniform, accumulate );
-            int hist_w = 512, hist_h = 400;
-            int bin_w = cvRound( (double) hist_w/histSize );
-            Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-            normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-            normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-            normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-            for( int i = 1; i < histSize; i++ )
-            {
-                line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ),
-                      Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-                      Scalar( 255, 0, 0), 2, 8, 0  );
-                line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ),
-                      Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-                      Scalar( 0, 255, 0), 2, 8, 0  );
-                line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ),
-                      Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-                      Scalar( 0, 0, 255), 2, 8, 0  );
-            }
-            imshow("Source image", src );
-            imshow("calcHist Demo", histImage );
-            waitKey();
-}
+        split(src, bgr_planes );
+        int histSize = 256;
+        float range[] = { 0, 255 }; //the upper boundary is exclusive
+        const float* histRange[] = { range };
+        bool uniform = true, accumulate = false;
+        Mat b_hist, g_hist, r_hist;
+        calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, histRange, uniform, accumulate );
+        calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, histRange, uniform, accumulate );
+        calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, histRange, uniform, accumulate );
+        int hist_w = 512, hist_h = 400;
+        int bin_w = cvRound( (double) hist_w/histSize );
+        Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+        normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+        normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+        normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+        for( int i = 1; i < histSize; i++ )
+        {
+            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ),
+                 Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+                 Scalar( 255, 0, 0), 2, 8, 0  );
+            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ),
+                 Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+                 Scalar( 0, 255, 0), 2, 8, 0  );
+            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ),
+                 Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+                 Scalar( 0, 0, 255), 2, 8, 0  );
+        }
+        imshow("Source image", src );
+        imshow("calcHist Demo", histImage );
+        waitKey();
+    }
     void task5(cv::Mat src){
         cvtColor( src, src, COLOR_BGR2GRAY );
-           Mat dst;
-           equalizeHist( src, dst );
-           imshow( "Source image", src );
-           imshow( "Equalized Image", dst );
-           waitKey();
-
+        Mat dst;
+        equalizeHist( src, dst );
+        imshow( "Source image", src );
+        imshow( "Equalized Image", dst );
+        waitKey();
+        
     }
 };
 
@@ -248,7 +248,7 @@ int main(int argc, char** argv) {
     cv::Mat img = cv::imread(argv[1]);
     cv::Mat img2 = cv::imread(argv[2]);
     cv::Mat image = cv::imread(argv[3]);
-     
+    
     Lab2 lab2;
     lab2.task5(image);
     
